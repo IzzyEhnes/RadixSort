5317f0ce861de532437410e69dbcbf2fe95865da
IzzyEhnes <izzyehnes@gmail.com>
2020-10-22 12:02:36 -0700

    Feature: Add RadixSort class

--- src/Main.java
+++ src/Main.java
@@ -144,11 +144,66 @@ class Queue<T>
 
 
 
-public class Main
+class RadixSort
+{
+
+    private Queue<Integer>[] qArray;
+
+    public RadixSort()
+    {
+        Queue<Integer> Q0 = new Queue<>();
+        Queue<Integer> Q1 = new Queue<>();
+        Queue<Integer> Q2 = new Queue<>();
+        Queue<Integer> Q3 = new Queue<>();
+        Queue<Integer> Q4 = new Queue<>();
+        Queue<Integer> Q5 = new Queue<>();
+        Queue<Integer> Q6 = new Queue<>();
+        Queue<Integer> Q7 = new Queue<>();
+        Queue<Integer> Q8 = new Queue<>();
+        Queue<Integer> Q9 = new Queue<>();
+
+        qArray = new Queue[]{Q0, Q1, Q2, Q3, Q4, Q5, Q6, Q7, Q8, Q9};
+    }
+
+
+
+    public void radixSort(int[] inArray)
+    {
+
+    }
+
+
+
+    public int getLeastSignificantDigit(int inInt)
+    {
+        while (inInt > 10)
+        {
+            inInt %= 10;
+        }
+
+        int LSD = inInt;
+
+        System.out.println(LSD);
+
+        return LSD;
+    }
+
+}
+
+
+
+
+
+
+class Driver
 {
 
     public static void main(String[] args)
     {
-	// write your code here
+        int[] array = {170, 45, 75, 90, 802, 2, 24, 66};
+
+        RadixSort r = new RadixSort();
+
+        r.getLeastSignificantDigit(123);
     }
 }
